d86ecb31302c11b71789bd861b09b20d7b8d5004
aparna <aparna.06cs03@gmail.com>
2022-06-01 17:10:58 +0530

    changes conf

---
+++ src/main/java/com/spring/k8client1/SampleConfiguration.java
@@ -0,0 +1,22 @@
+package com.spring.k8client1;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@ConfigurationProperties(prefix = "welcome")
+@Configuration
+public class SampleConfiguration {
+	
+
+
+	    private String message = "hello how r u";
+
+	    public String getMessage() {
+	        return message;
+	    }
+
+	    public void setMessage(String message) {
+	        this.message = message;
+	    }
+	}
+	
