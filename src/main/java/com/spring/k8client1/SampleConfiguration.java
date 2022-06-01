7debd18fab01ba33637a0da1dd9cd0dc0778b4b2
aparna <aparna.06cs03@gmail.com>
2022-06-01 23:16:52 +0530

    changes

--- src/main/java/com/spring/k8client1/SampleConfiguration.java
+++ src/main/java/com/spring/k8client1/SampleConfiguration.java
@@ -1,14 +1,14 @@
 package com.spring.k8client1;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 
 @ConfigurationProperties(prefix = "welcome")
+@RefreshScope
 @Configuration
 public class SampleConfiguration {
 	
-
-
 	    private String message = "hello how r u";
 
 	    public String getMessage() {
