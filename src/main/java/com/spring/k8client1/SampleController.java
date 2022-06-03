5a45e973a09fb9554c25cd9132b2ff9d8a021f11
aparna <aparna.06cs03@gmail.com>
2022-06-03 08:29:04 +0530

    done

--- src/main/java/com/spring/k8client1/SampleController.java
+++ src/main/java/com/spring/k8client1/SampleController.java
@@ -1,5 +1,6 @@
 package com.spring.k8client1;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,12 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SampleController {
 	
+	@Autowired 
+	private SampleConfiguration conf;
+	
 	 @Value("${message:testing kubectl}")
 	    private String message;
 	 
 	  @GetMapping
 	    public String welcome() {
 	        System.out.println(message);
+	        System.out.println("the controoler class->"+conf.getMessage());
 	        return message;
 	    }
 
